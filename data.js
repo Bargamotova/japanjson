d51d710794acf750b2b4672329bf66629eb69bb0
Bargamotova <anna.samoilenko.2012@gmail.com>
2024-03-26 14:10:11 +0900

    fix

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
-const apiUrl = 'https://bargamotova.github.io/japanjson/data';
+const apiUrl = 'https://bargamotova.github.io/japanjson/cities/';
 const data = [];
 
 fetch(apiUrl)
   .then((res) => res.json())
   .then((res) => {
-    data.push(res);
+    console.log(...res)
 
 
 

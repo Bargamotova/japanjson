760407441d91f159d2de13e38748188d3eb3215b
Bargamotova <anna.samoilenko.2012@gmail.com>
2024-03-30 17:36:34 +0900

    added checking code

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ fetch(apiMusicUrl)
     console.log(first.title)
     console.log(songs)
   })
+const apiCitiesUrl = 'https://bargamotova.github.io/japanjson/data/cities.json';
+const cities = [];
+
+fetch(apiCitiesUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    cities.push(...data);
+    const first = cities[1]
+    console.log(first.title)
+    console.log(cities)
+  })
